7cfee4d410382e0f85ae1a7e241cf7ba671ab1a6
Seb <sebastien.linck@univ-reims.fr>
2021-01-19 15:28:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require("include/header.html");
 $html = <<<HTML
 <main>
-    <h1>Test</h1>
+    <h1>Test Seb</h1>
     <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum quos excepturi voluptate doloremque, obcaecati consequatur? Iusto, in ullam ducimus laudantium, odit sequi cumque amet architecto nihil exercitationem, laborum debitis sit!
     </p>
