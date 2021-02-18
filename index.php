ab057900033caca2fd28d30f31b113b2f62238e2
thiffaine levilly <75316510+thiff812@users.noreply.github.com>
2021-02-18 09:54:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $html = <<<HTML
 <main>
     <h1>Test Seb</h1>
     <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum quos excepturi voluptate doloremque, obcaecati consequatur? Iusto, in ullam ducimus laudantium, odit sequi cumque amet architecto nihil exercitationem, laborum debitis sit!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum quos excepturi voluptate doloremque, obcaecati consequatur? Iusto, in ullam ducimus laudantium, odit sequi cumque amet architecto nihil exercitationem, laborum debitis sit! blabla
     </p>
 </main>
 HTML;
