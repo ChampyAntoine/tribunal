5fd60f57105feb59baa13851138ed7f65101dfd4
thiffaine levilly <75316510+thiff812@users.noreply.github.com>
2021-02-27 12:20:52 +0100

    Create palaisJustice.php

---
+++ palaisJustice.php
@@ -0,0 +1,43 @@
+<?php
+require("include/header.html");
+$html = <<<HTML
+
+<main>
+	<div id="tribunaux">
+		<h2>Palais de Justice (Site Esplanade)</h2>
+		<section class="section-tribunaux">
+			<article class="tribunal">
+				<div class="img-tribunal">
+					<img src="tribunal1.png">
+				</div>
+				<div id="texte-tribunal">
+					<h3>Aile Nord</h3>
+					<h3>Pôle famille</h3>
+					<p>Le palais de justice occupe le pôle famille avec le Juge des affaires
+					familiales. Celui-ci s'occupe principalement des divorces, des pensions familiales et de l'autorité parentale. Vous retrouverez le juge des Affaires Familiale au premier étage et sous-sol. De plus, le pôle famille possède le juge des enfants qui
+					est un magistrat spécialisé dans les problèmes de l'enfance en pénal au tribunal judiciaire. Il intervient en matière civile lorsqu'il est dit qu'un mineur est en dander, ou que sa santé, sa sécurité ou sa moralitée est menacée ou que les conditions de son éducation semble gravement compromises. Vous retrouverez ce service au Res de chaussée. </p>
+					<h3>Aile Est</h3>
+					<h3>Pôle pénal</h3>
+					<p>Le palais de justice propose 4 services au niveau pénal. Dans un premier il propose avec le Juge d'instruction chargé ces enquêtes judiciaires dans les affaires pénales les plus graves ou les plus complexes, deux missions: Procéder impartialement à la manifestation de la vérité et prendre certaines décisions juridicionnelles. Vous le trouverez au deuxième étage.</p>
+					<p> Dans un second temps, le palais de justice propose les services parquet autorité chargé de défendre l'intérêt de la collectivité et l'application de la loi devant les juridictions. Vous le trouverez au premier étage.</p>
+					<p> Dans un troisième temps, vous trouverez le Juge des Libertés et de la Détention au Res de chaussée qui va statuer sur une mise en détention provisoire d'une personne mise en examen ou sa demande de mise en liberté.</p>
+					<p>Enfin, vous trouverez le Juge de l'applicatoin des peines au Rez de chaussée qui opère un suivi des condamnés à l'intérieur et à l'extérieur de la prison.</p>
+				</div>
+			</article>
+			<article class="tribunal">
+				
+				<div id="coordonnees-tribunal">
+					<h3>Contact</h3>
+					<p>9 Espl. du Palais de Justice, 08000 Charleville-Mézières, France.</p>
+					<p>+33324576900</p>
+				</div>
+				<div class="img-tribunal">
+					<img src="tribunal1.png">
+				</div>
+			</article>
+			
+<main>
+HTML;
+
+echo($html);
+require("include/footer.html");
