abb431e866da18ef7aa87c02268e8d407b44b59f
thiffaine levilly <75316510+thiff812@users.noreply.github.com>
2021-03-08 15:31:02 +0100

    Update siteSevigne.php

--- siteSevigne.php
+++ siteSevigne.php
@@ -4,29 +4,37 @@ require("include/header.php");
 $html = <<<HTML
 
 <main>
-	<div id="tribunaux">
+	<div id="tribunaux" class="div">
 		<h2>Site Madame de Sévigné (ancien tribunal d'instance)</h2>
-		<section class="section-tribunaux">
-			<article class="tribunal">
-				<div class="img-tribunal">
-					<img src="tribunal2.png">
+		<section class="section">
+			<article class="art">
+				<div class="img-art">
+					<img src="./src/image/DSC_0034.JPG">
 				</div>
-				<div id="texte-tribunal">
+				</section>
+				<section class="section">
+				<div class="texte-tri">
 					<h4>Pôle civil</h4>
 					<p>Le pôle civil géré par le Juge de l'exécution et le Juge des contentieux de la protection hors de chambre de proximité, regrroupe les situations tel que la protection des majeurs, le bail d'habitation, le crédit à la consommation et le surendettement.</p>
 				</div>
 			</article>
+			</section>
 			<article class="tribunal">
 				
-				<div id="coordonnees-tribunal">
+				<section class="section">
+				<div class="texte-tri">
 					<h3>Contact</h3>
 					<p>12 Rue Madame de Sévigné, 08000 Charleville-Mézières, France</p>
 					<p>+33324563413</p>
 				</div>
-				<div class="img-tribunal">
-					<img src="tribunal2.png">
+				</section>
+				<section class="section">
+				<div class="img-art">
+					<img src="./src/image/carte-sevigne.png">
 				</div>
+				</section>
 			</article>
+			
 			
 <main>
 HTML;
